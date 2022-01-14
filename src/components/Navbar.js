ab5d53875cd6bc69e583df23c8e9fac16674c611
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-01-14 18:31:52 +0530

    removed right lin

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,19 +7,19 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="text-utils/">
+        <Link className="navbar-brand" to="/">
           {props.title}
         </Link>
         
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="text-utils/">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="text-utils/about">
+              <Link className="nav-link" to="/about">
                 About Us
               </Link>
             </li>
