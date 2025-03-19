31342556c5da9086c973f0ec3f7f70790ac08a7a
MOHD TAUHID <83533970+tauhidsaif@users.noreply.github.com>
2025-03-19 16:21:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,63 +4,51 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Use HashRouter
 
 function App() {
   const [mode, setmode] = useState("light");
   const [text, settext] = useState("ENABLE DARK MODE");
 
   const toggleBtn = () => {
-    if (mode === 'light') {
-      setmode('dark');
-      document.body.style.color = 'white';
-      document.body.style.backgroundColor = 'black';
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.color = "white";
+      document.body.style.backgroundColor = "black";
       settext("DISABLE DARK MODE");
       showAlert("Dark mode has been Enabled", "Success");
     } else {
-      setmode('light');
-      document.body.style.color = 'black';
-      document.body.style.backgroundColor = 'white';
-      settext("ENABLE DARK MODE")
-      showAlert("Light mode has been Enabled", "Success")
-      document.getElementsByClassName('accordion-button')[0].style.backgroundColor = 'white';
-
+      setmode("light");
+      document.body.style.color = "black";
+      document.body.style.backgroundColor = "white";
+      settext("ENABLE DARK MODE");
+      showAlert("Light mode has been Enabled", "Success");
     }
   };
 
-  const [alert, setalert] = useState(null)
+  const [alert, setalert] = useState(null);
 
   const showAlert = (message, type) => {
     setalert({
       message: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
-      setalert(null)
+      setalert(null);
     }, 1500);
-  }
+  };
 
   return (
-    <>
-      <Router>
-
-        <Navbar mode={mode} toggleBtn={toggleBtn} title="TextUtils" text={text} />
-        <Alert alert={alert} />
-        <div className="container my-3">
+    <Router basename="/text-utils"> {/* Add basename */}
+      <Navbar mode={mode} toggleBtn={toggleBtn} title="TextUtils" text={text} />
+      <Alert alert={alert} />
+      <div className="container my-3">
         <Routes>
-          <Route exact path="/" element={<TextForm showAlert={showAlert} mode={mode} heading="Enter the text below to Analyze" />}/>
-            
-
-          <Route exact path="/about" element={<About mode={mode} showAlert={showAlert} />}/>
-
+          <Route exact path="/" element={<TextForm showAlert={showAlert} mode={mode} heading="Enter the text below to Analyze" />} />
+          <Route exact path="/about" element={<About mode={mode} showAlert={showAlert} />} />
         </Routes>
-        </div>
-      </Router>
-    </>
+      </div>
+    </Router>
   );
 }
 
